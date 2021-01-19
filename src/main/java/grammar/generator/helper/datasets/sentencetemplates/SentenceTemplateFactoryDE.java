c15f085bde6c4e859906e13a26dd1070dfb98cc7
breglin <breglin@techfak.uni-bielefeld.de>
2021-01-19 13:59:43 +0100

    Added German base.ttl and sentence template

--- src/main/java/grammar/generator/helper/datasets/sentencetemplates/SentenceTemplateFactoryDE.java
+++ src/main/java/grammar/generator/helper/datasets/sentencetemplates/SentenceTemplateFactoryDE.java
@@ -3,6 +3,11 @@ package grammar.generator.helper.datasets.sentencetemplates;
 import grammar.generator.helper.datasets.Factory;
 import grammar.structure.component.Language;
 
+import java.util.List;
+
+import static grammar.generator.helper.datasets.sentencetemplates.SentenceTemplate.createNPTemplate;
+import static grammar.generator.helper.datasets.sentencetemplates.SentenceTemplate.createSentenceTemplate;
+
 class SentenceTemplateFactoryDE implements Factory<SentenceTemplateRepository> {
 
   private final SentenceTemplateRepository sentenceTemplateRepository;
@@ -23,5 +28,26 @@ class SentenceTemplateFactoryDE implements Factory<SentenceTemplateRepository> {
   }
 
   private void initDE(Language language) {
+    //NounPPFrame
+    sentenceTemplateRepository.add(
+      createSentenceTemplate(
+        language,
+        List.of(
+          "interrogativePronoun verb(reference:component_be) NP(prepositionalAdjunct)?"
+        ),
+        "copulativeArg",
+        "prepositionalAdjunct"
+      )
+    );
+    // NP(prepositionalAdjunct)
+    sentenceTemplateRepository.add(
+      createNPTemplate(
+        language,
+        List.of(
+          "determiner(reference:component_the) noun(root) preposition prepositionalAdjunct"
+        ),
+        "prepositionalAdjunct"
+      )
+    );
   }
 }
